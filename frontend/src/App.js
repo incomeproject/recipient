b3329597b143fff9f68ba61502db27b5e98a6beb
TheRoyalTnetennba <gpaye8@gmail.com>
2023-02-23 17:09:19 -0700

    run basic create-react-app for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>One day there will be things here</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders placeholder text', () => {
+  render(<App />);
+  const placeholder = screen.getByText(/One day there/i);
+  expect(placeholder).toBeInTheDocument();
+});
