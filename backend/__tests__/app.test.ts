14535bfe5032bec0c26be9abe9f9290d39c26485
TheRoyalTnetennba <gpaye8@gmail.com>
2023-04-04 08:02:10 -0600

    fix index route test

--- backend/__tests__/app.test.ts
+++ backend/__tests__/app.test.ts
@@ -5,6 +5,6 @@ import app from "../app";
 describe("Test app.ts", () => {
   test("Catch-all route", async () => {
     const res = await request(app).get("/");
-    expect(res.body).toEqual({ message: "Express + TypeScript Server" });
+    expect(res.body).toEqual({ message: "One day there will be things here." });
   });
 });
--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -2,7 +2,9 @@ import express, { Express, Request, Response, NextFunction } from "express";
 
 export const indexRouter = express.Router();
 
-indexRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
-  console.log("got request");
-  res.send("Express + TypeScript Server");
-});
+indexRouter.use(
+  "/",
+  (req: Request, res: Response, next: NextFunction): void => {
+    res.json({ message: "One day there will be things here." });
+  }
+);
