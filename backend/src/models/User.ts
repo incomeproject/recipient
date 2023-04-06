b41fd82a99e0464c4f26c758d50891075399adcc
Graham Paye <gpaye8@gmail.com>
2023-04-06 10:02:01 -0400

    use uuid and improve route error handling and tests (#8)

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -8,8 +8,8 @@ import {
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
   @Index()
   @Column({ unique: true, nullable: false })
